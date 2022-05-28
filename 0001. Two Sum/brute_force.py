74ec798668e11293fc60e6c96a9399afe28f4c17
joelescudefont <joelescudefont@gmail.com>
2022-05-28 13:12:35 +0200

    Add solution

---
+++ 0001. Two Sum/brute_force.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def two_sum(self, nums: List[int], target: int) -> List[int]:
+        """
+        Get the indices of the two elements that sum together to a given target.
+        """
+        for idx1, num1 in enumerate(nums):
+            for idx2, num2 in enumerate(nums[idx1 + 1:]):
+                if target == num1 + num2:
+                    return [idx1, idx2 + idx1 + 1]
+        else:
+            raise Exception('Indices were not found!')

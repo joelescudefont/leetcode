ac63134f8be2d06ef4286abf8de476d23c7bdabf
joelescudefont <joelescudefont@gmail.com>
2022-06-04 15:53:34 +0200

    Add solution

---
+++ 0206. Reverse Linked List/iterative.py
@@ -0,0 +1,18 @@
+from typing import Optional
+
+
+from utils import ListNode
+
+
+class Solution:
+    def reverse_linked_list(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        """
+        Reverse a singly-linked list.
+        """
+        prev = None
+        while head:
+            tmp = head.next
+            head.next = prev
+            prev = head
+            head = tmp
+        return prev

af56655ac9f90b46703a9df2ec44529888336223
Joel Escude Font <46526671+joelescudefont@users.noreply.github.com>
2023-08-14 16:09:19 +0200

    Create collections_counter_most_common.py
    Add solution

---
+++ 0347 Top K Frequent Elements/collections_counter_most_common.py
@@ -0,0 +1,22 @@
+from collections import Counter
+from typing import List
+
+import pytest
+
+
+class Solution:
+    @staticmethod
+    # Find the top most frequent elements
+    def find_top_k_frequent_elements(nums: List[int], k: int) -> List[int]:
+        return [elem for elem, _ in Counter(nums).most_common(k)]
+
+
+@pytest.mark.parametrize("expected,k,nums", [
+    ([], 1, []),
+    ([1], 1, [1]),
+    ([2], 1, [1, 2, 2, 3]),
+    ([2, 1], 2, [1, 2, 2, 3]),
+    ([2, 1, 3], 3, [1, 2, 2, 3]),
+])
+def test_find_top_k_frequent_elements(expected, k, nums):
+    assert set(expected) == set(Solution().find_top_k_frequent_elements(nums, k))

f64991e11923375957861b3498a4f4eb392588ed
joelescudefont <joelescudefont@gmail.com>
2024-03-26 15:21:01 +0100

    Add solution

--- 0062 Unique Paths/README.md
+++ 0062 Unique Paths/README.md
@@ -13,3 +13,7 @@ Recursion with backtracking and a counter
 Recursion with backtracking and a counter
 - Time complexity: `O(MN)`
 - Space complexity: `O(MN)`
+
+Dynamic programming with a 2D array using breath-first search
+- Time complexity: `O(MN)`
+- Space complexity: `O(MN)`
---
+++ 0062 Unique Paths/dynamic_programming_2d_array_bfs.py
@@ -0,0 +1,46 @@
+from collections import deque
+from typing import List, Set
+
+
+class Solution:
+    def __init__(self, m: int, n: int):
+        self.m: int = m
+        self.n: int = n
+
+        self.grid: List[List[int]] = [[0 for _ in range(n)] for _ in range(m)]
+
+        self.queue: deque = deque([(0, 0)])
+        self.seen: Set = set()
+
+    def count_unique_paths(self) -> int:
+        self.grid[0][0] += 1  # top left
+
+        self.bfs()  # traverse with breath-first search
+
+        return self.grid[self.m - 1][self.n - 1]
+
+    def bfs(self) -> None:
+        while self.queue:
+            tile = self.queue.popleft()
+
+            if tile not in self.seen:
+                self.seen.add(tile)  # save visited tile
+
+                i, j = tile
+
+                if i + 1 < self.m:
+                    self.grid[i + 1][j] += self.grid[i][j]  # update down
+                    self.queue.append((i + 1, j))
+
+                if j + 1 < self.n:
+                    self.grid[i][j + 1] += self.grid[i][j]  # update right
+                    self.queue.append((i, j + 1))
+
+        return
+
+
+# Test cases
+assert 2 == Solution(2, 2).count_unique_paths()
+assert 3 == Solution(2, 3).count_unique_paths()
+assert 6 == Solution(3, 3).count_unique_paths()
+assert 28 == Solution(3, 7).count_unique_paths()
